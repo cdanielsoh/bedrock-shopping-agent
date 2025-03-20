70b34bc2ac2fc883111eefecb956c70bdcce491a
cdanielsoh <chanhosoh@gmail.com>
2025-03-20 06:37:40 +0000

    DynamoDB CDK stack for user data

--- app.py
+++ app.py
@@ -4,9 +4,11 @@ import os
 import aws_cdk as cdk
 from stacks.personalize.personalize_stack import PersonalizeDataImportStack
 from stacks.opensearch.opensearch_stack import OpenSearchServerlessStack
-
+from stacks.dynamodb.dynamodb_stack import DynamoDBUserTableStack
 
 app = cdk.App()
 # PersonalizeDataImportStack(app, "PersonalizeDataImportStack")
-OpenSearchServerlessStack(app, "OpenSearchServerlessStack")
+# OpenSearchServerlessStack(app, "OpenSearchServerlessStack")
+DynamoDBUserTableStack(app, "DynamoDbStack")
+
 app.synth()
---
+++ stacks/dynamodb/dynamodb_stack.py
@@ -0,0 +1,44 @@
+from aws_cdk import (
+    Stack,
+    aws_dynamodb as dynamodb,
+    aws_s3 as s3,
+    aws_s3_deployment as s3deploy,
+    RemovalPolicy,
+    CfnOutput
+)
+from constructs import Construct
+
+class DynamoDBUserTableStack(Stack):
+    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+        super().__init__(scope, construct_id, **kwargs)
+
+        # Create S3 bucket for CSV data
+        csv_bucket = s3.Bucket(
+            self, "CSVBucket",
+            removal_policy=RemovalPolicy.DESTROY,
+            auto_delete_objects=True
+        )
+
+        # Upload CSV to S3 bucket
+        s3deploy.BucketDeployment(
+            self, "DeployCSV",
+            sources=[s3deploy.Source.asset("./data/dynamodb")],
+            destination_bucket=csv_bucket
+        )
+
+        # Create DynamoDB table
+        user_table = dynamodb.Table(
+            self, "UserTable",
+            partition_key=dynamodb.Attribute(name="id", type=dynamodb.AttributeType.STRING),
+            billing_mode=dynamodb.BillingMode.PAY_PER_REQUEST,
+            import_source=dynamodb.ImportSourceSpecification(
+                bucket=csv_bucket,
+                input_format=dynamodb.InputFormat.csv(),
+                key_prefix="raw_users.csv"
+            ),
+            removal_policy=RemovalPolicy.DESTROY
+        )
+
+        # Outputs
+        CfnOutput(self, "TableName", value=user_table.table_name)
+        CfnOutput(self, "BucketName", value=csv_bucket.bucket_name)
